18eb0e9173bfa4a46026cf6a07b6f9a101670ddb
Blagovest <blagovest.iliev.93@gmail.com>
2026-02-15 18:28:30 +0000

    Add Vercel Analytics for tracking

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../app/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
 
 export default MyApp;
